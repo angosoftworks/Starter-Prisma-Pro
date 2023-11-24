d0a02ea0588b7b6a2b36a8c0679c896b224ee63a
iqbal125 <iqbal125sf@gmail.com>
2023-11-24 15:37:45 -0800

    remove nextra docs

--- next.config.js
+++ next.config.js
@@ -1,16 +1,6 @@
-const withPlugins = require('next-compose-plugins');
-
-const withMDX = require('@next/mdx')();
-const withNextra = require('nextra')({
-  theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx']
 };
 
-//module.exports = withMDX(nextConfig);
-module.exports = withPlugins([withNextra], nextConfig);
-//module.exports = nextConfig;
+module.exports = nextConfig;
