2b7505f9af88625a41c2da8d05b67d427718e799
iqbal125 <iqbal125sf@gmail.com>
2024-01-08 13:51:16 -0800

    fix build errors

--- src/lib/API/Services/payments/stripe/subscription.ts
+++ src/lib/API/Services/payments/stripe/subscription.ts
@@ -1,41 +1,41 @@
-import stripe from '@/lib/API/Services/init/payments';
-import routes from '@/lib/config/routes';
-import Stripe from 'stripe';
+//import stripe from '@/lib/API/Services/init/payments';
+//import routes from '@/lib/config/routes';
+//import Stripe from 'stripe';
 
-import configuration from '@/lib/config/site';
+//import configuration from '@/lib/config/site';
 
-interface createProtalProps {
-  customer_id: string;
-}
+//interface createProtalProps {
+//  customer_id: string;
+//}
 
-export const GetBillingUrl = async ({ customer_id }: createProtalProps): Promise<string> => {
-  let portalSession: Stripe.BillingPortal.Session;
-  const customer = customer_id;
+//export const GetBillingUrl = async ({ customer_id }: createProtalProps): Promise<string> => {
+//  let portalSession: Stripe.BillingPortal.Session;
+//  const customer = customer_id;
 
-  const origin = configuration.url;
+//  const origin = configuration.url;
 
-  try {
-    portalSession = await stripe.billingPortal.sessions.create({
-      customer,
-      return_url: `${origin}${routes.redirects.user.toUserDashboard}`
-    });
-  } catch (err) {
-    throw err;
-  }
+//  try {
+//    portalSession = await stripe.billingPortal.sessions.create({
+//      customer,
+//      return_url: `${origin}${routes.redirects.user.toUserDashboard}`
+//    });
+//  } catch (err) {
+//    throw err;
+//  }
 
-  return portalSession.url;
-};
+//  return portalSession.url;
+//};
 
-export const RetrieveSubscription = async (
-  subscription_id: string
-): Promise<Stripe.Subscription> => {
-  let subscription: Stripe.Subscription;
+//export const RetrieveSubscription = async (
+//  subscription_id: string
+//): Promise<Stripe.Subscription> => {
+//  let subscription: Stripe.Subscription;
 
-  try {
-    subscription = await stripe.subscriptions.retrieve(subscription_id as string);
-  } catch (err) {
-    throw err;
-  }
+//  try {
+//    subscription = await stripe.subscriptions.retrieve(subscription_id as string);
+//  } catch (err) {
+//    throw err;
+//  }
 
-  return subscription;
-};
+//  return subscription;
+//};
